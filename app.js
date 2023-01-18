4a13b3226651e00a72156f00cf4f9558fd53ff9b
Utif Milkedori <utifmd@gmail.com>
2023-01-18 21:05:02 +0700

    event handling simply works

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require("express")
 const http = require("http")
+const socketIo = require("socket.io")
 
 const app = express()
 const server = http.Server(app)
+const io = socketIo(server)
 let port = process.env.PORT || 5000
 
 app.get(`/`, (req, resp) => {
     resp.sendFile('index.html', {root: __dirname})
 })
-
+io.on('connection', socket => {
+    console.log(`A user connected`)
+    
+    socket.on('disconnect', () => {
+        
+        console.log(`A user disconnected`)
+    })
+})
 server.listen(port, () => {
     console.log(`app listening on port ${port}`)
 })
